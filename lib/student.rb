baf7b6710c25a2a4c7503aae30112638de3f3565
pledgedavis <pledgedavis14@gmail.com>
2020-03-02 20:35:54 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,37 @@
+require "pry"
 class Student
+  attr_accessor :name, :grade 
+  attr_reader :id 
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade 
+    # Remember, you can access your database connection anywhere in this class
+    #  with DB[:conn]  
+  end
+    
+  def self.create_table
+      sql = <<-SQL 
+       CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade TEXT
+        ) 
+        SQL
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+        DB[:conn].execute(sql) 
+  end
+
+  def self.drop_table
+    DROP TABLE students;
+  end
+
+  def save
   
+  end
+
+  def self.create 
+  
+  end
+
+
 end
