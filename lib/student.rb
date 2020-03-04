9cd6ae23d2f962c10638e5c0f8235b9e990ccf33
github username <github email address>
2020-03-03 22:05:33 -0800

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,15 +5,15 @@
 class Student
 
   attr_accessor :name, :grade
-  attr_reader :id
+  attr_reader :id #responds to a getter for :id, does not provide a setter for :id
   
-  def initialize(name, grade, id = nil)
+  def initialize(name, grade, id = nil) #the name attribute can be accessed, the grade attribute can be accessed
     @name = name 
     @grade = grade
     @id = id
   end
 
-  def self.create_table
+  def self.create_table #creates the students table in the database
     sql = <<-SQL
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY,
@@ -24,13 +24,13 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def self.drop_table
+  def self.drop_table #drops the students table from the database
     sql = "DROP TABLE students"   # You can write it as a direct string or heredoc
     
     DB[:conn].execute(sql)
   end
   
-  def save
+  def save #saves an instance of the Student class to the database
     sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
 
     DB[:conn].execute(sql, self.name, self.grade)
@@ -38,10 +38,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0] #???
   end
 
-  def self.create(name:, grade:)
+  def self.create(name:, grade:) #takes in a hash of attributes and uses metaprogramming to create a new student object.
     student = Student.new(name, grade)
-    student.save
-    student
+    student.save #uses the #save method to save that student to the database
+    student #returns the new object that it instantiated
   end
 
 end
